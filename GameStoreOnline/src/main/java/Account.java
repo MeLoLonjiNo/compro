894adc3fb18ccaab78e9f06d4846ae3129a36664
Kritsanapon <kritsanapon.melo@mail.kmutt.ac.th>
2020-04-16 21:23:49 +0700

    add attribute and constructor getter setter tostring

--- GameStoreOnline/src/main/java/Account.java
+++ GameStoreOnline/src/main/java/Account.java
@@ -1,13 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author 66800
- */
-public class Account {
-    
+import java.util.Date;
+
+public class Account extends Person{
+    private String userID;
+    private String password;
+    private AccountStatus accountStatus;
+    private Person person;
+
+    public Account(String userID, String password, String name, String address, Date dateOfBirth, String email, String phone) {
+        super(name, address, dateOfBirth, email, phone);
+        this.userID = userID;
+        this.password = password;
+        this.accountStatus = AccountStatus.customer;
+    }
+
+    public Account(Person person, String name, String address, Date dateOfBirth, String email, String phone) {
+        super(name, address, dateOfBirth, email, phone);
+        this.person = person;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public AccountStatus getAccountStatus() {
+        return accountStatus;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setAccountStatus(AccountStatus accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "userID=" + userID + ", accountStatus=" + accountStatus + '}';
+    }
+
 }
--- GameStoreOnline/src/main/java/Person.java
+++ GameStoreOnline/src/main/java/Person.java
@@ -1,13 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author 66800
- */
-class Person {
+import java.util.Date;
+
+
+public class Person {
+    private String name;
+    private String address;
+    private Date dateOfBirth;
+    private String email;
+    private String phone;
+
+    public Person(String name, String address, Date dateOfBirth, String email, String phone) {
+        this.name = name;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", address=" + address + ", dateOfBirth=" + dateOfBirth + ", email=" + email + ", phone=" + phone + '}';
+    }
+    
+    
     
 }
