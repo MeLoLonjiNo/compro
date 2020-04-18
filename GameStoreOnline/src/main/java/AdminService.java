754cc78be185cf400749524540def53b897591ad
Chicky <nippit.c@mail.kmutt.ac.th>
2020-04-18 22:45:40 +0700

    แก้ AdminService

--- GameStoreOnline/src/main/java/AdminService.java
+++ GameStoreOnline/src/main/java/AdminService.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Nippit
- */
-public interface AdminService {
-    
+    public interface AdminService {
+        public String addProduct();
+        public String removeProduct();
+        public String banCustomer();
+        public String unBanCustomer();
+        public String addMoney();
+        public String addAdmin();
 }
