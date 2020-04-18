e0637d4aa11eff27feea73e68eb552450bb78ea8
Kritsanapon <kritsanapon.melo@mail.kmutt.ac.th>
2020-04-18 22:17:37 +0700

    ???? ตรวจสอบความคืบหน้า add&view ได้ใน MeLo_Test

---
+++ GameStoreOnline/src/main/java/MeLo_Test.java
@@ -0,0 +1,15 @@
+
+public class MeLo_Test {
+    public static void main(String[] args) {
+        Store store = new Store(10);
+        Product pd01 = new Product("PD01","Final Fantasy VII Remake","Game Form Square Enix",1000);
+        Product pd02 = new Product("PD02","Final Fantasy XV ","Game Form Square Enix",1500);
+        Product pd03 = new Product("PD03","Dota2","GG game",0);
+        
+        store.addProduct(pd01);
+        store.addProduct(pd02);
+        store.addProduct(pd03);
+        
+        store.viewShop();
+    }
+}
--- GameStoreOnline/src/main/java/Store.java
+++ GameStoreOnline/src/main/java/Store.java
@@ -6,13 +6,18 @@ public class Store implements CustomerService{
     private CustomerAccount[] customer;
     private int count;
 
+    public Store(int maxCapacity) {
+        this.store = new Product[maxCapacity];
+        
+    }
+    
     public void addProduct(Product pd){
         for (int i = 0; i<store.length; i++) {
             if(store[i]!=null){
                 this.count=i;
             }
         }
-        
+        this.store[count+1]=pd;
 //        for (int i = 0; i < store.length; i++) {
 //            if(store[i]!=null){ this.store[i]=pd; }
 //        }
@@ -21,7 +26,7 @@ public class Store implements CustomerService{
     @Override
     public String viewShop() {
         for (int i = 0; i < store.length ; i++) {
-            System.out.println("No."+(i+1 )+store[i].toString());
+            System.out.println("No."+(i+1)+""+store[i]);
         }return "_______________________________________";
     }
 
@@ -53,6 +58,11 @@ public class Store implements CustomerService{
     @Override
     public boolean register() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" + "store=" + store + '}';
     }
     
     
