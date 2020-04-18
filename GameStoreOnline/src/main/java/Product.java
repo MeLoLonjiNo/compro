1b805c254be8c6ef6a25804fae08aa3159d35891
Jirayut <jirayut.bal4ncez@mail.kmutt.ac.th>
2020-04-18 13:10:25 +0700

    เพิ่ม Product มี equal , toString

--- GameStoreOnline/src/main/java/Product.java
+++ GameStoreOnline/src/main/java/Product.java
@@ -1,3 +1,6 @@
+
+import java.util.Objects;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,68 @@
  * @author 66800
  */
 public class Product {
-    private String productCode;
+    private final String productCode;
+    private final String productName;
+    private final String description;
+    private final int price;
+    private final ProductStatus productStatus;
+    
+    public Product (String productCode , String productName , String description , int price) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.description = description;
+        this.price = price;
+        this.productStatus = ProductStatus.onSale;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public ProductStatus getProductStatus() {
+        return productStatus;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Product other = (Product) obj;
+        if (!Objects.equals(this.productCode, other.productCode)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return  "Product Code : " + productCode +
+                "Product Name : " + productName +
+                "Description : " + description +
+                "Price : " + price +
+                "Product Status : " + productStatus;
+    }
+    
+    
+    
+    
 }
