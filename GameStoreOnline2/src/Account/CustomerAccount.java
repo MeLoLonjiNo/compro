2b6d2373354b28e5c185f17f8d5fd8a512e42a2a
Kritsanapon <kritsanapon.melo@mail.kmutt.ac.th>
2020-04-23 21:51:27 +0700

    Update path Customer 7.25A

--- GameStoreOnline2/src/Account/CustomerAccount.java
+++ GameStoreOnline2/src/Account/CustomerAccount.java
@@ -15,12 +15,13 @@ public class CustomerAccount extends  Account{
         super(account);
         productInCart = new Product[100];
         storage = new Product[100];
+        money = 2000;
     }
     
     public CustomerAccount(String id , String password , Person person){
         super(id, password, person);
-        productInCart = new Product[20];
-        storage = new Product[20];
+        productInCart = new Product[100];
+        storage = new Product[100];
     }
     
     boolean addProductToChart(Product addingProduct){
@@ -34,8 +35,67 @@ public class CustomerAccount extends  Account{
         return true;
     }
     
-    int getProductInCartIndex(){
-        
+     boolean deleteProductToChart(Product deletingProduct){
+        if(deletingProduct==null){ return false; }
+        for (int i = 0; i < productInCart.length; i++) {
+            if(productInCart[i].equals(deletingProduct)){
+                productInCart[i] = null ;
+                break;
+            }
+        }
+        return true;
     }
+    
+    int getProductInCartIndex(Product searchProductInCart){
+        for (int i = 0; i < productInCart.length; i++) {
+            if(productInCart[i]!=null && searchProductInCart.equals(productInCart[i]) ){
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    boolean addProductToStorage(Product addingProduct){
+       if(addingProduct==null){ return false; }
+        for (int i = 0; i < storage.length; i++) {
+            if(storage[i]==null){
+                storage[i] = addingProduct;
+                break;
+            }
+        }
+        return true;
+    }
+    
+     boolean deleteProductToStorage(Product deletingProduct){
+        if(deletingProduct==null){ return false; }
+        for (int i = 0; i < storage.length; i++) {
+            if(storage[i].equals(deletingProduct)){
+                storage[i] = null ;
+                break;
+            }
+        }
+        return true;
+    }
+     
+     int payment(Product payingProduct){
+        if(payingProduct==null){return -1;}
+        if(money-payingProduct.getPrice()<0){return -2;}
+        else money = money-payingProduct.getPrice(); {return money;}
+        
+     }
+
+    Product[] getProductInCart() {
+        return productInCart;
+    }
+
+    int getMoney() {
+        return money;
+    }
+
+    Product[] getStorage() {
+        return storage;
+    }
+    
+    
     
 }
