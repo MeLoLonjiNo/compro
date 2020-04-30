a5524126255017262547c6775cd5dda362149d7d
Chicky <nippit.c@mail.kmutt.ac.th>
2020-04-30 22:37:21 +0700

    fix

--- GameStoreOnline2/src/MeLo_Test.java
+++ GameStoreOnline2/src/MeLo_Test.java
@@ -24,7 +24,7 @@ public class MeLo_Test {
         Product pd03 = new Product("PD03","Dota2","GG game",0);
         Product pd04 = new Product("PD04","Dead By Daylight","Game price 300 Bug 3Million",300);
         //Product pd05 = new Product("PD05",null,null,100);
-
+        //
         //store.addProduct(pd01);
         //store.addProduct(pd02);
         //store.addProduct(pd03);
