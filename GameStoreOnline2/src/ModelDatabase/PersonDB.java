33fc007562b2331df30e9da460a056ed2b429b6e
Jirayut <jirayut.bal4ncez@mail.kmutt.ac.th>
2020-05-02 23:05:37 +0700

    [ADD] PersonDB

--- GameStoreOnline2/src/ModelDatabase/PersonDB.java
+++ GameStoreOnline2/src/ModelDatabase/PersonDB.java
@@ -11,19 +11,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class PersonDB implements PersonInterface{
+public class PersonDB {
 
-    @Override
+    
     public int insert(Person obj) {
-        String sql = "INSERT INTO product VALUES(?,?,?,?,?)";
+        String sql = "INSERT INTO person VALUES(?,?,?,?,?,?,?)";
         int row = 0;
         try (Connection conn = DBConnection.getConnection();
                 PreparedStatement stm = conn.prepareStatement(sql)) {
-            stm.setString(1, obj.getProductCode());
-            stm.setString(2, obj.getProductName());
-            stm.setString(3, obj.getDescription());
-            stm.setInt(4, obj.getPrice());
-            stm.setString(5, obj.getProductStatusToString());
+            stm.setString(1, obj.getName());
+            stm.setString(2, obj.getAddress());
+            stm.setInt(3, obj.getDate());
+            stm.setInt(4, obj.getMonth());
+            stm.setInt(5, obj.getYear());
+            stm.setString(6, obj.getEmail());
+            stm.setString(7, obj.getPhone());
             row = stm.executeUpdate();
         } catch (SQLException ex) {
             System.out.println("Cannot insert in to dabase: " + ex.getMessage());
@@ -31,17 +33,19 @@ public class PersonDB implements PersonInterface{
         return row;
     }
 
-    @Override
-    public int update(Person prod) {
-        String sql = "UPDATE product SET pcode=?,pname=?,description=?, price=?,pStatus=? WHERE pcode=? ";
+    
+    public int update(Person obj) {
+        String sql = "UPDATE person SET name=?,address=?,dateOfBirth=?, monthOfBirth=?,yearOfBirth=?,email=?,phone=? WHERE name=? ";
         int row = 0;
         try (Connection conn = DBConnection.getConnection();
                  PreparedStatement stm = conn.prepareStatement(sql)) {
-            stm.setString(1, prod.getProductCode());
-            stm.setString(2, prod.getProductName());
-            stm.setString(3, prod.getDescription());
-            stm.setInt(4, prod.getPrice());
-            stm.setString(5, prod.getProductStatusToString());
+            stm.setString(1, obj.getName());
+            stm.setString(2, obj.getAddress());
+            stm.setInt(3, obj.getDate());
+            stm.setInt(4, obj.getMonth());
+            stm.setInt(5, obj.getYear());
+            stm.setString(6, obj.getEmail());
+            stm.setString(7, obj.getPhone());
             row = stm.executeUpdate();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
@@ -49,12 +53,12 @@ public class PersonDB implements PersonInterface{
         return row;
     }
 
-    @Override
+    
     public int delete(Person prod) {
         int row = 0;
         try (Connection conn = DBConnection.getConnection();
                 Statement stm = conn.createStatement()) {
-            String sql = "DELETE FROM product WHERE pcode=" + prod.getProductCode();
+            String sql = "DELETE FROM person WHERE name=" + prod.getName();
             row = stm.executeUpdate(sql);
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
@@ -62,15 +66,15 @@ public class PersonDB implements PersonInterface{
         return row;
     }
 
-    @Override
+    
     public GeneralList<Person> getAll() {
         GeneralList<Person> prods = new GeneralList<>();
         try (Connection conn = DBConnection.getConnection();
                 Statement stm = conn.createStatement()) {
-            String sql = "SELECT * FROM product";
+            String sql = "SELECT * FROM person";
             ResultSet rs = stm.executeQuery(sql);
             while (rs.next()) {
-                prods.add(new Person(rs.getString("pcode"), rs.getString("pname"), rs.getString("description"),rs.getInt("price"),rs.getString("pStatus")));
+                prods.add(new Person(rs.getString("name"), rs.getString("address"), rs.getInt("dateOfBirth"),rs.getInt("monthOfBirth"),rs.getInt("yearOfBirth"),rs.getString("email"),rs.getString("phone")));
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
@@ -78,33 +82,33 @@ public class PersonDB implements PersonInterface{
         return prods;
     }
 
-    @Override
-    public Person findById(int id) {
-        Product prod = null;
-        try (Connection conn = DBConnection.getConnection();
-                Statement stm = conn.createStatement()) {
-            String sql = "SELECT * FROM product where pro_id=" + id;
-            ResultSet rs = stm.executeQuery(sql);
-            if (rs.next()) {
-                prod = new Product(rs.getString("pcode"), rs.getString("pname"), rs.getString("description"),rs.getInt("price"),rs.getString("pStatus"));
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-        System.out.println(prod);
-        return prod;
-    }
+//    @Override
+//    public Person findById(int id) {
+//        Product prod = null;
+//        try (Connection conn = DBConnection.getConnection();
+//                Statement stm = conn.createStatement()) {
+//            String sql = "SELECT * FROM person where pro_id=" + id;
+//            ResultSet rs = stm.executeQuery(sql);
+//            if (rs.next()) {
+//                prod = new Product(rs.getString("pcode"), rs.getString("pname"), rs.getString("description"),rs.getInt("price"),rs.getString("pStatus"));
+//            }
+//        } catch (SQLException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//        System.out.println(prod);
+//        return prod;
+//    }
 
-    @Override
+    
     public GeneralList<Person> findByName(String name) {
-        GeneralList<Product> prodList = new GeneralList<>();
-        String sql = "SELECT * FROM product WHERE pro_name like ?";
+        GeneralList<Person> prodList = new GeneralList<>();
+        String sql = "SELECT * FROM person WHERE name like " + name;
         try (Connection conn = DBConnection.getConnection();
                 PreparedStatement stm = conn.prepareStatement(sql)) {
             stm.setString(1, "%" + name + "%");
             ResultSet rs = stm.executeQuery();
             while (rs.next()) {
-                prodList.add(new Product(rs.getString("pcode"), rs.getString("pname"), rs.getString("description"),rs.getInt("price"),rs.getString("pStatus")));
+                prodList.add(new Person(rs.getString("name"), rs.getString("address"), rs.getInt("dateOfBirth"),rs.getInt("monthOfBirth"),rs.getInt("yearOfBirth"),rs.getString("email"),rs.getString("phone")));
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
